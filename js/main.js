706fc71fb98072e628566ff20f99ff6a656c4d76
snehpatel200603 <121982229+snehpatel200603@users.noreply.github.com>
2023-06-06 16:52:03 -0400

    un finished code

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 console.log("JS file conneccted");
 
+
+
+const theButtons = document.querySelectorAll("#buttonHolder img"),
+puzzleboard = document.querySelector(".puzzle-board");
+//console.log(theButtons);
+console.log(puzzleboard);
+
+function changeBGImage() {
+    //console.log("changeBGImage calleed");
+    //url('../images/backGround0.jpg');
+    puzzleboard.style.backgroundImage = `url(images/backGround${this.id}.jpg)` 
+}
+
+
+//event listener
+theButtons.forEach(button => button.addEventListener("click", changeBGImage));
+
+puzzlePieces.forEach()
