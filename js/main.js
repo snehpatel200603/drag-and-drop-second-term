d50a83e45fc11a2ceca189e0c7200fe8e2bfb0f9
snehpatel200603 <121982229+snehpatel200603@users.noreply.github.com>
2023-06-13 16:09:59 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const theButtons = document.querySelectorAll("#buttonHolder img"),
     puzzleBoard = document.querySelector(".puzzle-board"),
     puzzlePieces = document.querySelectorAll(".puzzle-pieces img");
+    puzzlePiecesDiv = document.querySelector(".puzzle-pieces")
     dropZones = document.querySelectorAll(".drop-zone"); 
 //console.log(theButtons);
 //console.log(puzzleBoard);
@@ -12,6 +13,13 @@ function changeBGImage() {
     //console.log("changeBGImage called");
     //url('../images/backGround0.jpg');
     puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`
+
+    // bug fix 2 will go hear
+    // will use for each loop and if statement
+    // oi want ot loop through dropzones and check if there is?
+    //check with firstChild 
+    // if there is a child 
+    // puzzlePieceDiv.appendChild(something needs to go hear )
 }
 
 function handleDragOver(e) {
@@ -30,8 +38,14 @@ function handleDrop (e) {
     e.preventDefault();
     console.log("dorpped sonething on me");
     //this line moves the draged peace form the ;est sode of the board into ehatever dropzone we choose 
+    //bugfix 1 will go hear 
+if(this.children.length >= 1){
+  return;
+}
     this.appendChild(draggedPieces);
 }
+
+
 
 //event Listeners
 theButtons.forEach(button => button.addEventListener("click", changeBGImage)); 
